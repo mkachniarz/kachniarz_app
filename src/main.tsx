1ab11a1ee540106ff84abf3d94ded037a1134bd0
Maciej Kachniarz <mkachniarz1@gmail.com>
2024-05-03 15:01:02 +0200

    add menu

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import MenuBar from "./components/MenuBar";
+import "./index.css";
+import { StyledEngineProvider } from "@mui/material/styles";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <StyledEngineProvider injectFirst>
+      <MenuBar />
+      <App />
+    </StyledEngineProvider>
+  </React.StrictMode>
+);
